8dace1779790933d9fb6c44ae5b4dbe3d9299f6f
Luke Thinnes <lukethinnes@yahoo.com>
2021-08-12 21:33:19 -0600

    Added types.

--- Addition/index.js
+++ Addition/index.js
@@ -1,5 +1,3 @@
-console.log('working')
-
 const button = document.querySelector('button')
 const input1 = document.getElementById('num1')
 const input2 = document.getElementById('num2')
@@ -8,6 +6,6 @@ function add(num1, num2) {
   return num1 + num2
 }
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
   console.log(add(input1.value, input2.value))
 })
