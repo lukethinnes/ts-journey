a8614b1bd53d0a07140125ae152ea1b322bf09be
Luke Thinnes <lukethinnes@yahoo.com>
2021-08-18 20:02:17 -0600

    Updated.

--- Skeleton/app.ts
+++ Skeleton/app.ts
@@ -2,11 +2,20 @@
   //name: string;
   //age: number;
 //}
-const person = {
+const person: {
+  name: string
+  age: number
+  hobbies: string[]
+  role: [number, string]
+} = {
   name: 'Luke',
   age: 27,
-  hobbies: ['Cooking', 'Hiking']
+  hobbies: ['Cooking', 'Hiking'],
+  //tuples always have only 2 elements!
+  role: [5, 'Writer']
 }
+
+person.role.push('admin')
 
 let favoriteActivities: string[]
 favoriteActivities = ['Cooking']
